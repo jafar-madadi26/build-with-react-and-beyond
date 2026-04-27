e8f9ec56b03fa3c0416389b7913d3debba26a87c
Jafar Madadi <jafar.madadi2026@gmail.com>
2026-04-27 11:44:21 +0430

    Rendering Lists in React

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -2,6 +2,44 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import './index.css';
 
+const pizzaData = [{
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoPath: "pizzas/focaccia.jpg",
+    soldOut: false,
+}, {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoPath: "pizzas/margherita.jpg",
+    soldOut: false,
+}, {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoPath: "pizzas/spinaci.jpg",
+    soldOut: false,
+}, {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoPath: "pizzas/funghi.jpg",
+    soldOut: false,
+}, {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoPath: "pizzas/salamino.jpg",
+    soldOut: true,
+}, {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoPath: "pizzas/prosciutto.jpg",
+    soldOut: false,
+},];
+
 
 function App() {
     return <div className="container">
@@ -16,39 +54,39 @@ function Header() {
     // const style = {color: "red", fontSize: "42px", textTransform: "uppercase"};
     const style = {};
 
-    return (
-        <h1 className="header">
-            Fast React Pizza Co.
-        </h1>
-    );
+    return (<h1 className="header">
+        Fast React Pizza Co.
+    </h1>);
 }
 
 function Menu() {
-    return (
-        <main className="menu">
-            <h2>Our menu</h2>
-            <Pizza name='Pizza Spinaci' ingredients='Tomato, mozarella, spinach, and ricotta cheese'
-                   photoPath='pizzas/spinaci.jpg' price={10}/>
+    return (<main className="menu">
+        <h2>Our menu</h2>
 
-            <Pizza name='Pizza Funghai' ingredients='Tomato, mozarella, spinach, and ricotta cheese'
-                   photoPath={'pizzas/funghi.jpg'} price={23}/>
-        </main>
-    )
+        <ul className="pizzas">
+            {pizzaData.map((pizza => (
+                <Pizza pizzaOpj={pizza} key={pizza.name} />
+            )))}
+        </ul>
+
+        {/*<Pizza name='Pizza Spinaci' ingredients='Tomato, mozarella, spinach, and ricotta cheese'*/}
+        {/*       photoPath='pizzas/spinaci.jpg' price={10}/>*/}
+
+        {/*<Pizza name='Pizza Funghai' ingredients='Tomato, mozarella, spinach, and ricotta cheese'*/}
+        {/*       photoPath={'pizzas/funghi.jpg'} price={23}/>*/}
+    </main>)
 }
 
 function Pizza(props) {
-    console.log(props);
-
     return (
-        <div className='pizza'>
-            <img src={props.photoPath} alt={props.name}/>
+        <li className='pizza'>
+            <img src={props.pizzaOpj.photoPath} alt={props.pizzaOpj.name}/>
             <div>
-                <h3>{props.name}</h3>
-                <p>{props.ingredients}</p>
-                <span>{props.price + 3}</span>
+                <h3>{props.pizzaOpj.name}</h3>
+                <p>{props.pizzaOpj.ingredients}</p>
+                <span>{props.pizzaOpj.price}</span>
             </div>
-        </div>
-    );
+        </li>);
 }
 
 
@@ -63,16 +101,12 @@ function Footer() {
     // else alert("Sorry we're closed!");
 
 
-    return (
-        <footer className="footer">
-            {new Date().toLocaleDateString()}. We're currently open
-        </footer>
-    );
+    return (<footer className="footer">
+        {new Date().toLocaleDateString()}. We're currently open
+    </footer>);
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>
-);
+root.render(<React.StrictMode>
+    <App/>
+</React.StrictMode>);
