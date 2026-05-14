0d14e9feb985ad03da8e25f92f32fc51d65b48d9
Jafar Madadi <jafar.madadi2026@gmail.com>
2026-05-14 09:55:05 +0430

    Basic Conditional Rendering With &&

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -60,14 +60,21 @@ function Header() {
 }
 
 function Menu() {
+
+    const pizzas = pizzaData;
+    // const pizzas = [];
+    const numPizzas = pizzas.length;
+
     return (<main className="menu">
         <h2>Our menu</h2>
 
-        <ul className="pizzas">
-            {pizzaData.map((pizza => (
-                <Pizza pizzaOpj={pizza} key={pizza.name}/>
-            )))}
-        </ul>
+        {numPizzas > 0 && (
+            <ul className="pizzas">
+                {pizzas.map((pizza => (
+                    <Pizza pizzaOpj={pizza} key={pizza.name}/>
+                )))}
+            </ul>)}
+
 
         {/*<Pizza name='Pizza Spinaci' ingredients='Tomato, mozarella, spinach, and ricotta cheese'*/}
         {/*       photoPath='pizzas/spinaci.jpg' price={10}/>*/}
